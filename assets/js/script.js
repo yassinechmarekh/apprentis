2a452251b6ed376d2e9fce7b050639341d597993
yassine <yassinechmarekh88@gmail.com>
2024-08-25 13:48:26 +0100

    finish to hero section

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// hero-sec swip
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+    },
+    autoplay: {
+      delay: 2500,
+    },
+    loop: true,
+});
