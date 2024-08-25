6d6bf48c2ee622eafa6bba275913ca8c7c79675a
yassine <yassinechmarekh88@gmail.com>
2024-08-25 20:33:53 +0100

    finish tarif and contact sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,40 @@
 // hero-sec swip
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullests: true,
+var swiper = new Swiper(".slide-swp", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullests: true,
+  },
+  autoplay: {
+    delay: 2500,
+  },
+  loop: true,
+});
+
+// plans swip
+var swiper = new Swiper(".plans", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullests: true,
+  },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  loop: true,
+  breakpoints: {
+    1500: {
+      slidesPerView: 4,
     },
-    autoplay: {
-      delay: 2500,
+    1200: {
+      slidesPerView: 3,
     },
-    loop: true,
+    550: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
 });
