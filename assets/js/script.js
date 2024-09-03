7f828cc3064650927d06219822e0732567d71206
yassine <yassinechmarekh88@gmail.com>
2024-09-03 17:20:26 +0100

    config the contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,27 +76,44 @@ back_to_top.addEventListener("click", function () {
 });
 
 // contact form
-function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "yassineok288@gmail.com",
-    Password: "",
-    To: "yassineok288@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: document.getElementById("subject").value,
-    Body: document.getElementById("message").value,
-  }).then(() => {
-    const form = document.querySelector(
-      ".contact_sec .contact_form .content_contact form"
-    );
-    const alert = document.querySelector(
-      ".contact_sec .contact_form .content_contact .alert-success"
-    );
+const form = document.querySelector(
+  ".contact_sec .contact_form .content_contact form"
+);
+const alert = document.querySelector(
+  ".contact_sec .contact_form .content_contact .alert-success"
+);
 
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+function sendEmail() {
+  const BodyMessage = `
+    Nom : ${name.value} <br>
+    Email : ${email.value} <br>
+    Sujet : ${subject.value} <br>
+    Message : ${message.value} <br>
+  `;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username : "yassinechmarekh88@gmail.com",
+    Password : "05EC02FB29BA45BE780038FCF7D8A7B0E8C9",
+    To: "yassinechmarekh88@gmail.com",
+    From: "yassinechmarekh88@gmail.com",
+    Subject: subject.value,
+    Body: BodyMessage,
+  }).then(() => {
     form.classList.add("send");
     alert.classList.add("active");
   });
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
 
 // loader
 window.addEventListener("load", function () {
